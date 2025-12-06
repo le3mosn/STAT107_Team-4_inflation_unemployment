a7326fe1c760c014724bf8348e2da5762ef773c7
Vincent <vincentchau45678@gmail.com>
2025-12-05 20:15:57 -0800

    Load additional education dataset from remote source
    Added loading of education data from a remote CSV file.

--- Scripts/01_data_load.R
+++ Scripts/01_data_load.R
@@ -6,7 +6,9 @@ fred_inflation <- read_csv("data/T10YIE.csv")
 fred_unemploy  <- read_csv("data/UNRATE.csv")
 wb_inflation   <- read_csv("data/API_FP.CPI.TOTL.ZG_DS2_en_csv_v2_130173.csv", skip=4)
 wb_unemploy <- read_csv("data/API_SL.UEM.TOTL.NE.ZS_DS2_en_csv_v2_125709.csv", skip = 4)
+edu_data <- read.csv("https://raw.githubusercontent.com/zief0002/rustic-penguin/main/data/state-education.csv")
 list(fred_inflation = dim(fred_inflation),
      fred_unemploy = dim(fred_unemploy),
      wb_inflation = dim(wb_inflation),
      wb_unemploy = dim(wb_unemploy))
+
